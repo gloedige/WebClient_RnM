f97c0a9503b0e4d3541b6f9496888dcdc6717079
Gerd Lödige <gerd.loedige@posteo.de>
2023-06-02 11:31:17 +0200

    first commit

---
+++ src/main/java/de/iav/webclient_rnm/WebClientRnMApplication.java
@@ -0,0 +1,13 @@
+package de.iav.webclient_rnm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientRnMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientRnMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/iav/webclient_rnm/WebClientRnMApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iav.webclient_rnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientRnMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
